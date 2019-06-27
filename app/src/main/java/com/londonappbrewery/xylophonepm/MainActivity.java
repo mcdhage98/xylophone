75b5bbbd8483f8368347e5daf8fc671259ca720d
mayurcdhage <mayurcdhage@users.noreply.github.com>
2019-06-27 17:05:00 +0530

    done

--- app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private int mGSoundId;
     private int mASoundId;
     private int mBSoundId;
+    private SoundPool mSoundPool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +32,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // TODO: Create a new SoundPool
+        mSoundPool =new SoundPool(7,AudioManager.STREAM_MUSIC,0);
 
 
         // TODO: Load and get the IDs to identify the sounds
+        mCSoundId = mSoundPool.load(getApplicationContext(),R.raw.note1_c,1);
+        mDSoundId = mSoundPool.load(getApplicationContext(),R.raw.note2_d,1);
+        mESoundId = mSoundPool.load(getApplicationContext(),R.raw.note3_e,1);
+        mFSoundId = mSoundPool.load(getApplicationContext(),R.raw.note4_f,1);
+        mGSoundId = mSoundPool.load(getApplicationContext(),R.raw.note5_g,1);
+        mASoundId = mSoundPool.load(getApplicationContext(),R.raw.note6_a,1);
+        mBSoundId = mSoundPool.load(getApplicationContext(),R.raw.note7_b,1);
 
 
     }
 
     // TODO: Add the play methods triggered by the buttons
+public void playC(View v)
+{
+mSoundPool.play(mCSoundId,1.0F,1.0F,0,0,1.0F);
+}
+    public void playD(View v)
+    {
+        mSoundPool.play(mDSoundId,1.0F,1.0F,0,0,1.0F);
+    }
+
+    public void playE(View v)
+    {
+        mSoundPool.play(mESoundId,1.0F,1.0F,0,0,1.0F);
+    }
+
+    public void playF(View v)
+    {
+        mSoundPool.play(mFSoundId,1.0F,1.0F,0,0,1.0F);
+
+    }
+    public void playG(View v)
+    {
+        mSoundPool.play(mGSoundId,1.0F,1.0F,0,0,1.0F);
+    }
+
+
+    public void playA(View v)
+    {
+        mSoundPool.play(mASoundId,1.0F,1.0F,0,0,1.0F);
+    }
+
+    public void playB(View v)
+    {
+        mSoundPool.play(mBSoundId,1.0F,1.0F,0,0,1.0F);
+    }
 
 
 
